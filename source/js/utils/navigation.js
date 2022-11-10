211b4f8cdacd7e57b841f16aad3933160ff85672
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-11-11 01:10:12 +0400

    refactoring

--- source/js/utils/navigation.js
+++ source/js/utils/navigation.js
@@ -1,21 +1,18 @@
 // Mobile menu toggle
 const navMenuOpen = () => {
-  let navMain = document.querySelector('.header__navigation');
-  let navToggle = document.querySelector('.navigation__button');
   let mainBody = document.querySelector('.body');
-  let header = document.querySelector('.header');
+  let pageWrapper = document.querySelector('.wrapper');
+  let navToggle = document.querySelector('.navigation__button');
 
   mainBody.classList.remove('body--nojs');
 
   navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('header__navigation--closed')) {
-      navMain.classList.remove('header__navigation--closed');
-      navMain.classList.add('header__navigation--opened');
-      header.classList.add('header--opened');
+    if (pageWrapper.classList.contains('menu--closed')) {
+      pageWrapper.classList.remove('menu--closed');
+      pageWrapper.classList.add('menu--opened');
     } else {
-      navMain.classList.add('header__navigation--closed');
-      navMain.classList.remove('header__navigation--opened');
-      header.classList.remove('header--opened');
+      pageWrapper.classList.add('menu--closed');
+      pageWrapper.classList.remove('menu--opened');
     }
   });
 };
