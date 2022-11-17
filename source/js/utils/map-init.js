23fa76eb7f4cd60720e4ff81ed015d21ee417081
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-11-17 16:34:07 +0400

    add custom marker to yMap

--- source/js/utils/map-init.js
+++ source/js/utils/map-init.js
@@ -1,13 +1,24 @@
+/* eslint-disable no-undef */
 const mapInit = () => {
-  function init() {
-    // eslint-disable-next-line no-unused-vars, no-undef
+  ymaps.ready(function () {
     let myMap = new ymaps.Map('map', {
-      center: [59.939, 30.322],
+      center: [59.939666, 30.323111],
       zoom: 16,
+    }, {
+      searchControlProvider: 'yandex#search',
     });
-  }
-  // eslint-disable-next-line no-undef
-  ymaps.ready(init);
+
+    let myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {
+      hintContent: 'Собственный значок метки',
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: './img/marker.png',
+      iconImageSize: [18, 22],
+      iconImageOffset: [-5, -38],
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
 };
 
 export {mapInit};
