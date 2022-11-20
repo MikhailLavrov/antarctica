97ccd24b03a303e4938a8f79a925aab6b6470f8a
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-11-21 00:09:47 +0400

    remove upward button on footer

--- source/js/utils/upward.js
+++ source/js/utils/upward.js
@@ -1,4 +1,6 @@
-const scrollThumb = document.querySelector('.upward');
+const body = document.body;
+const scrollButton = document.querySelector('.upward');
+const footer = document.querySelector('.footer');
 
 function scrollToTop() {
   window.scrollTo({
@@ -7,12 +9,30 @@ function scrollToTop() {
 }
 
 const upwardButtonInit = () => {
-  if (scrollThumb) {
+  if (scrollButton) {
     window.addEventListener('scroll', function () {
-      scrollThumb.classList.toggle('active', window.scrollY > 500);
+
+      const viewportHeight = window.innerHeight;
+      const scrollProgress = Math.round(window.scrollY);
+      const footerHeight = footer.offsetHeight;
+      const bodyHeight = body.offsetHeight;
+      const heightBeforeFooter = bodyHeight - (viewportHeight + footerHeight);
+
+      // eslint-disable-next-line no-console
+      console.log(`1. viewportHeight: ${viewportHeight}`);
+      // eslint-disable-next-line no-console
+      console.log(`2. scrollProgress: ${scrollProgress}`);
+      // eslint-disable-next-line no-console
+      console.log(`3. bodyHeight: ${bodyHeight}`);
+      // eslint-disable-next-line no-console
+      console.log(`4. footerHeight: ${footerHeight}`);
+      // eslint-disable-next-line no-console
+      console.log(`5. if ${scrollProgress} >= ${heightBeforeFooter}`);
+
+      scrollButton.classList.toggle('active', scrollProgress > 500 && scrollProgress <= heightBeforeFooter);
     });
 
-    scrollThumb.addEventListener('click', scrollToTop);
+    scrollButton.addEventListener('click', scrollToTop);
   }
 };
 
